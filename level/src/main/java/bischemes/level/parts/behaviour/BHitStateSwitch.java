2a8379c8c8a677ccc0f53f1fcd8b00df5d042ea8
ch315stand <ch315@st-andrews.ac.uk>
2023-05-04 19:48:34 +0100

    Added State Switch on Collision behaviour

--- level/src/main/java/bischemes/level/parts/behaviour/BHitStateSwitch.java
+++ level/src/main/java/bischemes/level/parts/behaviour/BHitStateSwitch.java
@@ -1,10 +1,23 @@
 package bischemes.level.parts.behaviour;
 
 import bischemes.engine.GObject;
+import bischemes.level.parts.RObject;
 
 public class BHitStateSwitch extends BHit {
+
+    private final RObject switcher;
+
+    private BHitStateSwitch(RObject switcher) {
+        this.switcher = switcher;
+        switcher.addOnHit(this);
+    }
+
+    public static BHitStateSwitch assign(RObject switcher) {
+        return new BHitStateSwitch(switcher);
+    }
+
     @Override
     public void run(GObject hit) {
-
+        switcher.switchState();
     }
 }

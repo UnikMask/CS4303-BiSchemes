d2ce5dd41073e6b7518d6707ebaacd31011fb420
UnikMask <alexandre.guerville@hotmail.com>
2023-05-06 17:33:39 +0100

    game: [WIP] Player controller

---
+++ game/src/main/java/bischemes/game/Player.java
@@ -0,0 +1,17 @@
+package bischemes.game;
+
+import bischemes.engine.GObject;
+import processing.core.PVector;
+
+public class Player extends GObject {
+
+	public void update() {
+		// TODO Player Controls
+		// 1. Check movements
+	}
+
+	public Player(PVector position, float rotation) {
+		super(null, position, rotation);
+	}
+
+}

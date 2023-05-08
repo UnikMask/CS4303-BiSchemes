f7f3b6c9aa2ab7f1bc50f829b193e09dc0e38ea6
UnikMask <alexandre.guerville@hotmail.com>
2023-05-08 18:39:11 +0100

    engine: gravity: Return direction and tangent

--- engine/src/main/java/bischemes/engine/physics/ForceGenerators/DirectionalGravity.java
+++ engine/src/main/java/bischemes/engine/physics/ForceGenerators/DirectionalGravity.java
@@ -9,11 +9,24 @@ import processing.core.PVector;
 public class DirectionalGravity implements ForceGenerator {
 	private static final double GRAVITY_DEFAULT_COEFF = 9.81;
 	private static final PVector GRAVITY_DEFAULT_DIRECTION = new PVector(0, -1f);
+	private static final PVector CROSS_Z = new PVector(0, 0, 1);
 	double coeff;
 	PVector direction;
 
 	public void updateForce(RigidBody b) {
 		b.addForce(PVector.mult(direction, (float) (b.getMass() * coeff)));
+	}
+
+	public PVector getDirection() {
+		return direction;
+	}
+
+	public PVector getTangent() {
+		return CROSS_Z.cross(direction);
+	}
+
+	public void setDirection(PVector direction) {
+		this.direction = direction.copy().normalize();
 	}
 
 	public DirectionalGravity() {

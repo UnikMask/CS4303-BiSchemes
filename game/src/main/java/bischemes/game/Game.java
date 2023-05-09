4f870b36f3bbfd5ff4d633650869b428cd37da11
UnikMask <alexandre.guerville@hotmail.com>
2023-05-09 13:04:05 +0100

    game: [WIP] Room transition & object loading

--- game/src/main/java/bischemes/game/Game.java
+++ game/src/main/java/bischemes/game/Game.java
@@ -2,21 +2,26 @@ package bischemes.game;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayDeque;
 
 import bischemes.engine.*;
 import bischemes.engine.physics.*;
 import bischemes.engine.physics.ForceGenerators.DirectionalGravity;
 import bischemes.game.Game.GameState;
-import bischemes.level.Level;
-import bischemes.level.Room;
+import bischemes.level.parts.*;
+import bischemes.level.*;
 import processing.core.PApplet;
 import processing.core.PGraphics;
 import processing.core.PVector;
 
 public class Game {
+	// Constants
+	private static final double TRANSITION_DURATION = 0.5;
+
 	EngineRuntime engine;
 	GameState state = GameState.PLAY;
 
+	// General game components
 	Player player;
 	DirectionalGravity gravity;
 	boolean playerInPrimary = true;
@@ -26,10 +31,21 @@ public class Game {
 	Pair<Integer> colours;
 	List<Room> rooms;
 	Room currentRoom;
+	GObject primaryNode;
+	boolean isPrimaryScene;
+
+	// Transition components
+	Room nextRoom;
+	GObject nextRoomNode;
+	PVector previousPlayerPosition;
+	PVector targetPlayerPosition;
+	PVector previousCameraPosition;
+	PVector targetCameraPosition;
+	double timer = 0;
 
 	// States of a level/game - feel free to modify
 	enum GameState {
-		PAUSE, PLAY, INTRO, FINISH, END
+		PAUSE, PLAY, TRANSITION, INTRO, FINISH, END
 	}
 
 	public void update(PGraphics g) {
@@ -43,6 +59,17 @@ public class Game {
 				engine.update();
 				gravity.updateForce(player.getRigidBody());
 				break;
+			case TRANSITION:
+				timer += (1.0 / 60.0) / TRANSITION_DURATION;
+
+				if (timer < 1) {
+					engine.setCameraPosition(PVector.lerp(previousCameraPosition, targetCameraPosition, (float) timer));
+					player.setLocalPosition(PVector.lerp(previousPlayerPosition, targetPlayerPosition, (float) timer));
+				} else {
+					unloadLastRoom();
+					timer = 0;
+				}
+				engine.update();
 			case INTRO:
 				break;
 			case FINISH:
@@ -74,31 +101,114 @@ public class Game {
 		colours = new Pair<>(level.getColourPrimary(), level.getColourSecondary());
 
 		Room initRoom = level.getInitRoom();
-		loadRoom(initRoom);
 		gravity = new DirectionalGravity();
 		player = new Player(initRoom.getSpawnPosition(), 0, gravity, level.getColourSecondary());
+		loadRoom(initRoom);
 		secondaryScene.attachToGObject(secondaryScene.scene, player);
+		engine.setPause(false);
 	}
 
 	public void loadRoom(Room room) {
-		// Immediate room loading
-		if (currentRoom == null) {
-			// Initialise scenes
-			currentRoom = room;
-			primaryScene.grid = new GridSector(room.getDimensions(), new PVector(),
-					(int) room.getDimensions().x, (int) room.getDimensions().y);
-			VisualAttribute primaryBg = VisualUtils.makeRect(room.getDimensions(), colours.a);
-			primaryBg.setOffset(PVector.div(room.getDimensions(), 2));
-			//primaryScene.scene.addVisualAttributes(primaryBg); // TODO renenable background
-			secondaryScene.grid = new GridSector(room.getDimensions(), new PVector(),
-					(int) room.getDimensions().x, (int) room.getDimensions().y);
+		// Initialise scenes
+		currentRoom = room;
+		primaryScene.grid = new GridSector(room.getDimensions(), new PVector(),
+				(int) room.getDimensions().x, (int) room.getDimensions().y);
+		primaryNode = new GObject(null, PVector.div(room.getDimensions(), 2), 0);
+		VisualAttribute primaryBg = VisualUtils.makeRect(room.getDimensions(),
+				colours.a);
+		primaryNode.addVisualAttributes(primaryBg);
+		primaryScene.attachToGObject(primaryScene.scene, primaryNode);
+		secondaryScene.grid = new GridSector(room.getDimensions(), new PVector(),
+				(int) room.getDimensions().x, (int) room.getDimensions().y);
 
-			// Add geometries to both scenes
-			primaryScene.attachToGObject(primaryScene.scene, room.getPrimaryGeometry());
-			secondaryScene.attachToGObject(secondaryScene.scene, room.getSecondaryGeometry());
+		// Add geometries to both scenes
+		primaryScene.attachToGObject(primaryNode, room.getPrimaryGeometry());
+		secondaryScene.attachToGObject(secondaryScene.scene, room.getSecondaryGeometry());
+
+		// Load objects into the game
+		ArrayDeque<RObject> q = new ArrayDeque<>(room.getObjects());
+		while (!q.isEmpty()) {
+			RObject o = q.pollFirst();
+			if (o.getLColour() == null) {
+				q.addAll(o.getChildren().stream().map((go) -> (RObject) go).toList());
+			} else {
+				switch (o.getLColour()) {
+					case PRIMARY -> {
+						primaryScene.attachToGObject(primaryNode, o);
+					}
+					case SECONDARY -> {
+						secondaryScene.attachToGObject(secondaryScene.scene, o);
+					}
+				}
+			}
 		}
+	}
+
+	private void loadNextRoom(Room room, Adjacency adj, PVector direction, boolean isPrimaryScene) {
+		engine.setPause(true);
+		nextRoom = room;
+
+		// Compute transition vectors
+		PVector nextRoomPosition = PVector.add(
+				PVector.add(PVector.div(room.getDimensions(), 2), primaryNode.getPosition()),
+				PVector.mult(direction, PVector.dot(PVector.div(currentRoom.getDimensions(), 2), direction)));
+
+		previousPlayerPosition = player.getLocalPosition().copy();
+		targetPlayerPosition = PVector.add(adj.getLocalPosition(),
+				PVector.mult(direction, PVector.dot(Player.PLAYER_SIZE, direction)));
+
+		previousCameraPosition = engine.getCameraPosition();
+		targetCameraPosition = PVector.add(engine.getCameraPosition(), nextRoomPosition);
+
+		// Set the next room's node
+		nextRoomNode = new GObject(null, nextRoomPosition, 0);
+		nextRoomNode.addVisualAttributes(VisualUtils.makeRect(room.getDimensions(), colours.a));
+		primaryScene.attachToGObject(primaryScene.scene, nextRoomNode);
+
+		// Attach next room components to next room
+		secondaryScene.attachToGObject(secondaryScene.scene, room.getPrimaryGeometry());
+		state = GameState.TRANSITION;
+	}
+
+	private void unloadLastRoom() {
+		deconstructCurrentRoom();
+		loadRoom(nextRoom);
+		player.setLocalPosition(targetPlayerPosition);
+		if (isPrimaryScene) {
+			primaryScene.attachToGObject(primaryScene.scene, player);
+		} else {
+			secondaryScene.attachToGObject(secondaryScene.scene, player);
+		}
+		deconstructNextRoom();
+		state = GameState.PLAY;
 		engine.setPause(false);
 	}
+
+	/////////////////
+	// Destructors //
+	/////////////////
+
+	private void deconstructCurrentRoom() {
+		primaryScene = null;
+		secondaryScene = null;
+		primaryNode = null;
+		currentRoom = null;
+	}
+
+	private void deconstructNextRoom() {
+		nextRoom = null;
+		nextRoomNode = null;
+
+		previousPlayerPosition = null;
+		targetPlayerPosition = null;
+
+		previousCameraPosition = null;
+		targetCameraPosition = null;
+	}
+
+	//////////////////
+	// Constructors //
+	//////////////////
 
 	public Game(PApplet applet, PGraphics g) {
 		engine = new EngineRuntime(applet, g);
--- level/src/main/java/bischemes/level/parts/behaviour/BInteract.java
+++ level/src/main/java/bischemes/level/parts/behaviour/BInteract.java
@@ -9,104 +9,111 @@ import processing.core.PVector;
 
 public abstract class BInteract extends BUpdate {
 
-    protected final RObject interactable;
-    private final boolean useCircleProx;
-    private float xRange;
-    private float yRange;
-    private float radius;
+	protected final RObject interactable;
+	private final boolean useCircleProx;
+	private float xRange;
+	private float yRange;
+	private float radius;
 
-    private static final float INDICATOR_SCALE_RATE = 0.05f;
-    protected VisualAttribute indicator = null;
-    private boolean showingIndicator = false;
-    private float indicatorScale = 0f;
+	private static final float INDICATOR_SCALE_RATE = 0.05f;
+	protected VisualAttribute indicator = null;
+	private boolean showingIndicator = false;
+	private float indicatorScale = 0f;
 
-    private boolean activeOnState = false;
-    private boolean stateActivity;
+	private boolean activeOnState = false;
+	private boolean stateActivity;
 
-    protected BInteract(RObject interactable, float x, float y) {
-        this.interactable = interactable;
-        useCircleProx = false;
-        xRange = x;
-        yRange = y;
-        interactable.addOnUpdate(this);
-    }
+	protected BInteract(RObject interactable, float x, float y) {
+		this.interactable = interactable;
+		useCircleProx = false;
+		xRange = x;
+		yRange = y;
+		interactable.addOnUpdate(this);
+	}
 
-    protected BInteract(RObject interactable, float r) {
-        this.interactable = interactable;
-        useCircleProx = true;
-        radius = r;
-        interactable.addOnUpdate(this);
-    }
+	protected BInteract(RObject interactable, float r) {
+		this.interactable = interactable;
+		useCircleProx = true;
+		radius = r;
+		interactable.addOnUpdate(this);
+	}
 
-    public void addIndicator(PVector indicatorOffset) {
-        indicator = VisualUtils.makeTexturedPolygon(
-                new PVector(1, 1), 4, 0, indicatorOffset, SpriteLoader.getInteractSymbol());
-    }
+	public void addIndicator(PVector indicatorOffset) {
+		indicator = VisualUtils.makeTexturedPolygon(
+				new PVector(1, 1), 4, 0, indicatorOffset, SpriteLoader.getInteractSymbol());
+	}
 
-    public void setActiveOnState(boolean activeOnState) {
-        this.activeOnState = activeOnState;
-        this.stateActivity = true;
-    }
+	public void setActiveOnState(boolean activeOnState) {
+		this.activeOnState = activeOnState;
+		this.stateActivity = true;
+	}
 
-    // Checks whether a position is close enough to the interactable to interact
-    protected boolean canInteract(PVector position) {
-        if (activeOnState && (stateActivity != interactable.getState())) return false;
-        if (useCircleProx) {
-            float distance = position.dist(interactable.getPosition());
-            return distance < radius;
-        } else {
-            PVector distance = position.copy().sub(interactable.getPosition());
-            if (distance.x < 0) distance.x *= -1;
-            if (distance.y < 0) distance.y *= -1;
-            return distance.x < xRange && distance.y < yRange;
-        }
-    }
+	// Checks whether a position is close enough to the interactable to interact
+	protected boolean canInteract(PVector position) {
+		if (activeOnState && (stateActivity != interactable.getState()))
+			return false;
+		if (useCircleProx) {
+			float distance = position.dist(interactable.getPosition());
+			return distance < radius;
+		} else {
+			PVector distance = position.copy().sub(interactable.getPosition());
+			if (distance.x < 0)
+				distance.x *= -1;
+			if (distance.y < 0)
+				distance.y *= -1;
+			return distance.x < xRange && distance.y < yRange;
+		}
+	}
 
-    // Checks whether the InputHandler currently holds the InputCommand INTERACT
-    private boolean isInteraction() {
-        // TODO need to get interaction from room
-        return false;
-    }
+	// Checks whether the InputHandler currently holds the InputCommand INTERACT
+	private boolean isInteraction() {
+		// TODO need to get interaction from room
+		return false;
+	}
 
-    private void updateIndicator(boolean showIndicator) {
-        if (showIndicator) {
-            if (!showingIndicator) {
-                interactable.addVisualAttributes(indicator);
-                indicatorScale = INDICATOR_SCALE_RATE;
-                showingIndicator = true;
-            } else if (indicatorScale < 1f) {
-                indicatorScale += INDICATOR_SCALE_RATE;
-                if (indicatorScale > 1f)
-                    indicatorScale = 1f;
-            }
-            indicator.setScaling(indicatorScale);
-        } else if (showingIndicator) {
-            indicatorScale -= INDICATOR_SCALE_RATE;
-            showingIndicator = indicatorScale >= 0f;
-            if (showingIndicator)
-                indicator.setScaling(indicatorScale);
-            else
-                interactable.removeVisualAttributes(indicator);
-        }
-    }
+	private void updateIndicator(boolean showIndicator) {
+		if (showIndicator) {
+			if (!showingIndicator) {
+				interactable.addVisualAttributes(indicator);
+				indicatorScale = INDICATOR_SCALE_RATE;
+				showingIndicator = true;
+			} else if (indicatorScale < 1f) {
+				indicatorScale += INDICATOR_SCALE_RATE;
+				if (indicatorScale > 1f)
+					indicatorScale = 1f;
+			}
+			indicator.setScaling(indicatorScale);
+		} else if (showingIndicator) {
+			indicatorScale -= INDICATOR_SCALE_RATE;
+			showingIndicator = indicatorScale >= 0f;
+			if (showingIndicator)
+				indicator.setScaling(indicatorScale);
+			else
+				interactable.removeVisualAttributes(indicator);
+		}
+	}
 
-    protected abstract void onInteraction();
+	protected abstract void onInteraction();
 
-    @Override
-    public void run() {
-        GObject player = null;
-        // TODO need a way to actually get the player's GObject
-        PVector playerPos = player.getPosition();
-        boolean canInteract = canInteract(playerPos);
-        if (canInteract && isInteraction()) onInteraction();
-        if (indicator != null) updateIndicator(canInteract);
-    }
+	@Override
+	public void run() {
+		return;
 
-    @Override
-    public void setColour(int colour) {
-        if (indicator == null || showingIndicator) return;
-        indicator.makeTintedTexture(colour);
-    }
+		/*
+		 * GObject player = null;
+		 * // TODO need a way to actually get the player's GObject
+		 * PVector playerPos = player.getPosition();
+		 * boolean canInteract = canInteract(playerPos);
+		 * if (canInteract && isInteraction()) onInteraction();
+		 * if (indicator != null) updateIndicator(canInteract);
+		 */
+	}
 
+	@Override
+	public void setColour(int colour) {
+		if (indicator == null || showingIndicator)
+			return;
+		indicator.makeTintedTexture(colour);
+	}
 
 }

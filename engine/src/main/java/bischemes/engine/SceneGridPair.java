0d47d683a89b157fad20cb7bcd33a315363d546e
UnikMask <alexandre.guerville@hotmail.com>
2023-05-09 21:18:02 +0100

    engine: Method to reset scene with new grid

--- engine/src/main/java/bischemes/engine/SceneGridPair.java
+++ engine/src/main/java/bischemes/engine/SceneGridPair.java
@@ -3,6 +3,7 @@ package bischemes.engine;
 import java.util.Arrays;
 import java.util.ArrayDeque;
 import java.util.HashSet;
+import processing.core.PVector;
 
 import bischemes.engine.physics.GridSector;
 import bischemes.engine.physics.RigidBody;
@@ -44,6 +45,12 @@ public class SceneGridPair {
 			bodies.remove(b);
 			grid.remove(b);
 		}
+	}
+
+	public void resetScene(GridSector newGrid) {
+		scene = new GObject(null, new PVector(), 0);
+		bodies = new HashSet<>();
+		grid = newGrid;
 	}
 
 	public SceneGridPair(GObject scene, GridSector grid) {
